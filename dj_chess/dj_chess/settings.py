7c919f1c133b8a0e302f46bd42f0cd7e98b7be26
Dmitry Zharikov <zdimon77@gmail.com>
2021-04-21 13:00:49 +0300

    commit from zdimon77@gmail.com ----

--- dj_chess/dj_chess/settings.py
+++ dj_chess/dj_chess/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)q@cn@g4ukpimlfpg5-wlamvz!cmnzhv442r##f7yjh9qp62z!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
