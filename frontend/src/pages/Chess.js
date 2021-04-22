25d328b64fc105b8f6c2bf63d999591cf9d42209
Your Name <you@example.com>
2021-04-22 15:09:46 +0300

    commit from zdimon77@gmail.com ----

--- frontend/src/pages/Chess.js
+++ frontend/src/pages/Chess.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React , { useState, useEffect } from 'react';
+
+import Request from '../Request';
+
 
 function Chess() {
-  
+  useEffect(() => { 
+    console.log('sssssss');
+    var r = new Request();
+    r.get('/static/db.json')
+    .then((payload) => {
+      console.log(payload);
+    });
+  }, [])
   return (
     <div className="Chess" >
        <h1>Chess</h1>
