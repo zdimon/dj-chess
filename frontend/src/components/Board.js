5093debf1ac754c96e7f52176dc135adc19c187a
Your Name <you@example.com>
2021-04-28 16:21:26 +0300

    auto

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -1,10 +1,14 @@
 import './Board.css';
 import React from 'react';
 import Cell from './Cell';
+import io from 'socket.io-client'
+import {config} from '../config';
 
 
 function Board(props) {
   console.log(props);
+  var socket = io(`${config.backendURL}`)
+  console.log(socket);
   return (
     <div className="Board" >
        {props.board.map((el,index) => el.map((e,index) => <Cell cell={e} />)
